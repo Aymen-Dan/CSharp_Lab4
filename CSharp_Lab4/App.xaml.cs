e655dbc5b21f568dfa856414421dca797c720f47
Dan <stickbugdanny@gmail.com>
2024-04-05 16:03:45 +0300

    Add project files.

---
+++ CSharp_Lab4/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSharp_Lab4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// 
+    //to make a commit
+
+    public partial class App : Application
+    {
+    }
+
+}
