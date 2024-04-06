e3a76de6cf05b7a69667268e58cb8189223863b0
Dan <stickbugdanny@gmail.com>
2024-04-06 03:09:06 +0300

    Create AddPersonWindow.xaml.cs

---
+++ CSharp_Lab4/AddPersonWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using CSharp_Lab2_ParsonApp;
+using System.Windows;
+
+namespace CSharp_Lab4
+{
+    public partial class AddPersonWindow : Window
+    {
+        public AddPersonWindow()
+        {
+            InitializeComponent();
+            DataContext = new PersonViewModel();
+        }
+    }
+}
